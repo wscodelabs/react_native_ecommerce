1b7f4a9860b633fe10be3dddb1e12c3ada5ac563
Piyush Thapa <piyushthapa74@gmail.com>
2016-08-19 15:02:03 +0545

    added native-base

--- index.ios.js
+++ index.ios.js
@@ -7,47 +7,31 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View
+  View,
+  Navigator
 } from 'react-native';
+import Home from './src/components/Home'
+import ProductDetails from './src/components/ProductDetails'
+
+const routeComponents = {home:Home,productDetails:ProductDetails}
 
 class HelloWorld1 extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Navigator
+      style={{ flex:1 }}
+      initialRoute={{ name: 'home' }}
+      renderScene={ this.renderScene } />
     );
+  }
+
+  renderScene(route , navigator){
+    let Component = routeComponents[route.name]
+    console.log(Component);
+    return <Component navigator={navigator}/>
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
 
 AppRegistry.registerComponent('HelloWorld1', () => HelloWorld1);
